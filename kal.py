43be64d08e0338a0e79a4ce6679014b8141456f2
mohamedsayed18 <mohamed95.a.s@gmail.com>
2021-08-06 19:31:41 +0300

    clean the kal class

--- kal.py
+++ kal.py
@@ -1,23 +1,17 @@
 """
-Kalman filter
+Kalman filter, This file contain the class of the Kalman filter
 """
 import numpy as np
 
 from filterpy.kalman import KalmanFilter
-from filterpy.kalman import predict, update
-#from filterpy.kalman import update
-#from filterpy.kalman import predict
-from filterpy.common import Q_discrete_white_noise
-
 
 class kalman_matrix:
       def __init__(self):
-            self.robot_filter = KalmanFilter(dim_x=9, dim_z=6)
-            self.dt = .2
-            self.x = np.zeros((9,1))   # 6x1 matrix assuming everything is zero
+            self.robot_filter = KalmanFilter(dim_x=9, dim_z=6)    # Filter object
+            self.dt = .2      # rate of messages(sensor readings)
+            self.x = np.zeros((9,1))   # 9x1 matrix assuming everything is zero
             
-
-            # 6X9 matrix
+            # 6X9 matrix, measurement transation matrix
             self.h = np.array([[0, 0, 0, 1, 0, 0, 0, 0, 0],
                                [0, 0, 0, 0, 1, 0, 0, 0, 0],
                                [0, 0, 0, 0, 0, 1, 0, 0, 0],
@@ -25,23 +19,19 @@ class kalman_matrix:
                                [0, 0, 0, 0, 0, 0, 0, 1, 0],
                                [0, 0, 0, 0, 0, 0, 0, 0, 1]])
                               
-            #R = np.zeros((6,6))  # dim=6x6, orientation_covariance and linear_acceleration_covariance
+            # dim=6x6, orientation_covariance and linear_acceleration_covariance
             self.R = np.array([[0.0015387262937311438, 0, 0, 0, 0, 0],
                                [0, 0.0015387262937311438, 0, 0, 0, 0],
                                [0, 0, 0.0015387262937311438, 0, 0, 0],
                                [0, 0, 0, 0.002741552146694444, 0, 0],
                                [0, 0, 0, 0, 0.002741552146694444, 0],
-                               [0, 0, 0, 0, 0, 0.007615422629706791],
-                              
-                  ])
-      
-            self.z = np.array([5, 1, 1, 1.206141797081249, -0.46564058622688687, -1.819779513185261])
+                               [0, 0, 0, 0, 0, 0.007615422629706791]])
 
+            # 9x9 state variance matrix
             self.p = np.diag([50, 50, 50, 100, 100, 100, 5, 5, 5])
 
-        #self.q = np.identity(7)
+            # 9x9 process noise matrix, set to zero for simplicity 
             self.q = np.zeros((9, 9))
-        #self.u = np.array([9.74722984, 0.09411442, 0.63091934])
 
       def create_filter(self):
             self.robot_filter.x = self.x
@@ -54,40 +44,3 @@ class kalman_matrix:
             self.robot_filter.R[:]= self.R
             self.robot_filter.P[:] = self.p
             self.robot_filter.Q[:]= self.q
- 
-
-km = kalman_matrix()
-km.create_filter()
-
-#print(km.z)
-
-xs, cov = [], []
-for i in range(10):
-      km.robot_filter.predict()
-      km.robot_filter.update(km.z)
-      xs.append(km.robot_filter.x)
-      cov.append(km.robot_filter.P)
-      
-xs = np.array(xs)
-print(xs[:,3:])
-
-"""
-for _ in range(10):
-      km.x, km.p = predict(km.x, km.p, km.f, u=km.u, B=km.robot_filter.B)
-      print(km.x)
-
-
-v, _ = update(km.x, km.p, km.z, km.R, km.h)
-print(v)
-print(km.z)
-"""
-
-# Notes
-"""
-linear_acceleration_covariance: [0.0015387262937311438, 0.0, 0.0, 0.0, 0.0015387262937311438, 0.0, 0.0, 0.0, 0.0015387262937311438]
-orientation_covariance: [0.002741552146694444, 0.0, 0.0, 0.0, 0.002741552146694444, 0.0, 0.0, 0.0, 0.007615422629706791]
-
-The first message is at: 8:43:19
-time step is 0.2602640190000000131
-"""
-
