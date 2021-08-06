32239406b8acf0439cb3942dba16a39c44f0b482
mohamedsayed18 <mohamed95.a.s@gmail.com>
2021-08-06 20:35:32 +0300

    clean the listener node

--- listener.py
+++ listener.py
@@ -1,96 +1,49 @@
 #!/usr/bin/env python3
 
-import rospy
+import numpy as np
 
+import rospy
 from sensor_msgs.msg import Imu
 from geometry_msgs.msg import PoseStamped
-from geometry_msgs.msg import TransformStamped
-
-from kal import kalman_matrix
-from tf import transformations
 from tf.transformations import euler_from_quaternion
 from tf.transformations import quaternion_from_euler
 from tf import TransformBroadcaster
 
-import numpy as np
+from kal import kalman_matrix
 
-# Global variable TODO: Avoid global variables
+
+# Create the kalman filter
 km = kalman_matrix()
 km.create_filter()
 
-def callback(data):
+def Imu_callback(data):
     """
     Callback to Imu
     1. get the new measurements
     2. perform update
     """
+    # Get the measurements
     orientation_q = [data.orientation.x, data.orientation.y, data.orientation.y, data.orientation.w]
-    print("orientation_q", orientation_q)
+    # Change them to euler angles
     euler_angles = euler_from_quaternion(orientation_q)
-    print("euler angles", euler_angles)
+    # measurement which contain the linear acceleration followed by orientation
     reading = np.array([data.linear_acceleration.x, data.linear_acceleration.y, data.linear_acceleration.z,
                     euler_angles[0], euler_angles[1], euler_angles[2]])
     
-    km.robot_filter.predict()
-    km.robot_filter.update(reading)
-    #data.orientation.x, data.orientation.y, data.orientation.z, data.orientation.w])
-    print(km.robot_filter.x)
-    #move(km.robot_filter.x)
-    """
-    trans_message = TransformStamped()
-    trans_message.header.stamp = data.header.stamp
-    trans_message.header.frame_id = data.header.frame_id
+    km.robot_filter.predict()   # predict, calculate the prior
+    km.robot_filter.update(reading) # update, takes the sensor the measurement
 
-    trans_message.transform.translation.x = km.robot_filter.x[0]
-    trans_message.transform.translation.y = km.robot_filter.x[1]
-    trans_message.transform.translation.y = km.robot_filter.x[2]
+    print(km.robot_filter.x)    # the new states
 
-    trans_message.transform.rotation.x = 0
-    trans_message.transform.rotation.y = 0
-    trans_message.transform.rotation.z = 0
-    trans_message.transform.rotation.w = 1
-    """
-    br = TransformBroadcaster()
-    quat = quaternion_from_euler(*km.robot_filter.x[6:])
-    quat = quat/np.linalg.norm(quat)
-    no_rot = (0, 0, 0, 1)
-#(km.robot_filter.x[6], km.robot_filter.x[7], km.robot_filter.x[8], km.robot_filter.x[9])
+    br = TransformBroadcaster() # broadcast transformation between two frames
+    quat = quaternion_from_euler(*km.robot_filter.x[6:])    # Change orientation to quaternion
+    quat = quat/np.linalg.norm(quat)    # get the norm due to some computation accuracy 
+    
+    # send the transformation
     br.sendTransform(km.robot_filter.x[0:3], quat, data.header.stamp, "bobcat_base", "World")
-"""
-def move(states):
-    #pub = rospy.Publisher('/move_base_simple/goal', PoseStamped, queue_size=10)
-    #rate = rospy.Rate(10) # 10hz
-
-    # Create broadcaster
-    br = TransformBroadcaster()
-    br.
-    br.sendTransform((states[0], states[1], states[2]), 
-    (0, 0, 0, 1), rospy.Time.now(), "bobcat_base", "world")
-
-
-    while not rospy.is_shutdown():
-        br.sendTransform((2, 2, 0), (0,0,0,1), rospy.Time.now(), "bobcat_base", "world")
-        rate.sleep()
-    
-    
-    # The message to sent 
-    movecommand = PoseStamped()
-    movecommand.pose.position.x = 2
-    movecommand.pose.position.y = 2
-    movecommand.pose.position.z = 0
-     
-    while not rospy.is_shutdown():
-        pub.publish(movecommand)
-        rate.sleep()
-"""
-
-def listener():
-    rospy.init_node('listener', anonymous=True)
-    rospy.Subscriber("/imu/data", Imu, callback)    # TODO handle the rate of callback
-
-    # spin() simply keeps python from exiting until this node is stopped
-    rospy.spin()
 
 if __name__ == '__main__':
-    #rospy.init_node('Moverobot', anonymous=True)
-    listener()
+    
+    rospy.init_node('listener', anonymous=True)
+    rospy.Subscriber("/imu/data", Imu, Imu_callback)
+    rospy.spin()
